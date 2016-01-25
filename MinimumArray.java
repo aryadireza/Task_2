57259b5fbf4ce7f16ffaf5e6d640519201a15abb
Reza Aryadi - 1301144179 <aryav.reza08@gmail.com>
2016-01-25 17:44:10 +0700

    1301144179/1301140295

--- MinimumArray.java
+++ MinimumArray.java
@@ -1,16 +1,53 @@
-public class MinimumArray{
+/* IF-38-09
+1. Reza Aryadi (1301144179)
+2. Fitrah Bima Nusantara (1301140295)
+*/
 
-  public int checkMinimum(int[] A, int[] B){
+ public class MinimumArray{
+ 
+   public int checkMinimum(int[] A, int[] B){
     // write your solution here
-  }
-    
-  public static void main(String[] args){
-    MimimumArray m = new MinimumArray();
-    
+	int maxLength;
+	if(A.length>=B.length) {
+		maxLength=A.length;
+	}
+	else {
+		maxLength=B.length;
+	}
+	int[] temp = new int[maxLength];
+	int counter = 0;
+	for(int i=0;i<A.length;i++) {
+		for(int j=0;j<B.length;j++) {
+			if(A[i]==B[j]) {
+				temp[counter]=A[i];
+				counter++;
+			}
+		}
+	}
+	int min = 6666666;
+	for(int k=0;k<temp.length;k++) {
+		if(temp[k] != 0) {
+			if(temp[k] < min)  {
+				min = temp[k];
+			}
+		}	
+	}
+	if(min == 6666666) {
+		return -1;
+	}
+	else {
+		return min;
+	}
+   }
+     
+   public static void main(String[] args){
+    MinimumArray m = new MinimumArray();
+     
     int[] A = { 1, 3, 2, 1 };
     int[] B = { 4, 2, 5, 3, 2 };
-    
-    int result = m.checkMinimum( A, B );
-    
-  }
-}
+	
+     int result = m.checkMinimum( A, B );
+	System.out.println("Angka Terkecil : "+result);
+	
+   }
+ }
--- OddOccurences.java
+++ OddOccurences.java
@@ -1,16 +1,41 @@
-public class OddOccurences{
-  
-  public int checkOddOccurences( int[] A ){
+/* IF-38-09
+1. Reza Aryadi (1301144179)
+2. Fitrah Bima Nusantara (1301140295)
+*/
+ public class OddOccurences{
+   
+   public int checkOddOccurences( int[] A ){
     // write your solution here
-  }
-  
-  public static void main(String[] args){
-    OddOccurences odd = new OddOccurences();
-    
-    int[] A = { 9, 3, 9, 3, 9, 7, 9 };
-    
-    int result = odd.checkOddOccurences( A );
-    
-  }
-  
-}
+    int[] temp = new int[A.length];
+		for (int a=0; a<A.length; a++) {
+			temp[a] = 0;
+		}
+		for (int b=0; b<A.length; b++){
+			if (temp[b]==0) {
+				for (int c=b+1; c<A.length; c++){
+					if (A[b]==A[c]){
+						temp[b] = 1;
+						temp[b] = 1;
+					}
+				}
+			}
+		}
+		for (int d=0; d<=A.length; d++) {
+			if (temp[d]==0){
+				return(A[d]);
+			}
+		}
+  return 0;
+   }
+   
+   public static void main(String[] args){
+     OddOccurences odd = new OddOccurences();
+     
+     int[] A = { 9, 3, 9, 3, 9, 7, 9 };
+     
+     int result = odd.checkOddOccurences( A );
+	System.out.println("Angka yang tidak punya pasangan adalah = " + result);
+     
+   }
+   
+ }
